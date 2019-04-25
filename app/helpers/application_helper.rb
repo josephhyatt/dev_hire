c6eebe46266519f3f851f4c6a8e232ac0017550f
Joseph Hyatt <josephnhyatt@gmail.com>
2019-04-25 13:20:09 -0700

    add image to meetings views, and admin privilages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+
+  # Makes the time more visually pleasent
+  def readable_time(time)
+    time.strftime("%b %d, %I:%M%P")
+  end
+
+  # Checks to see if user is author of content
+  def author_of(record)
+    user_signed_in? && current_user.id == record.user_id
+  end
+
+  # Check if a user is an admin
+  def admin?
+    user_signed_in? && current_user.admin? 
+  end
 end
