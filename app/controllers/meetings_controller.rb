88395287c081d9770e76943daa2e5dfce79c405d
Joseph Hyatt <josephnhyatt@gmail.com>
2019-04-25 14:00:14 -0700

    generated calendar

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -1,6 +1,6 @@
 class MeetingsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_meeting, only: [:edit, :update, :show, :destory]
+  before_action :set_meeting, only: [:show, :edit, :update, :destory]
   
   def index
     # Accepts meetings from current use thats logged in
@@ -10,22 +10,41 @@ class MeetingsController < ApplicationController
   def show
   end
 
+  def new
+    @meeting = Meeting.new
+  end
+
   def edit
   end
 
   def create
     @meeting = Meeting.new(meeting_params)
     @meeting.user_id = current_user.id
+    if @meeting.save
+      flash[:notice] = "Meeting was successfully created."
+      redirect_to meeting_path(@meeting)
+    else
+      render 'show'
+    end
   end
 
-  def update
-  end
+	def update
+		if @meeting.update(meetingss_params)
+			flash.keep[:notice] = "Meeting was successfully updated."
+			redirect_to meeting_path(@meeting)
+		else
+			render 'edit'
+		end
+	end
 
   def destroy
     @meeting.destroy
+    flash[:notice] = "Meeting was successfully cancelled and deleted."
+		redirect_to meetings_path
   end
 
   private
+  
   # A callback to share common code (DRY)
   def set_meeting
     @meeting = Meeting.find(params[:id])
