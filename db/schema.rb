4530fbbac94af97253149bbb6b97a6bef66617e4
Joseph Hyatt <josephnhyatt@gmail.com>
2019-04-25 17:42:45 -0700

    added payment field for users migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_25_215842) do
+ActiveRecord::Schema.define(version: 2019_04_26_004151) do
 
   create_table "comments", force: :cascade do |t|
     t.text "reply"
@@ -54,6 +54,11 @@ ActiveRecord::Schema.define(version: 2019_04_25_215842) do
     t.boolean "admin", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "stripe_id"
+    t.string "card_brand"
+    t.string "car_exp_month"
+    t.string "card_exp_year"
+    t.string "card_last4"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
